4aad0737ef3998e4ea47859112c06f37cc59cd9f
AlexW-578 <github@avali.win>
2025-06-21 01:24:18 +0100

    Add creation of the Apprise notifier to __init__.py

--- wolnut/__init__.py
+++ wolnut/__init__.py
@@ -1,7 +1,10 @@
 import logging
+from wolnut.apprise import apprise_notifier
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
 )
 logger = logging.getLogger("wolnut")
+
+notifier = apprise_notifier("wolnut")
