ddbe242f8b2a22517409067d1c7542305f9bee0b
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2025-06-22 20:19:37 -0300

    Move entrypoint to cli.py

--- wolnut/__init__.py
+++ wolnut/__init__.py
@@ -1,15 +1,4 @@
 __version__ = '1.0.0'
-from .__main__ import main  # re-export
+from wolnut.cli import main  # re-export
 
-__all__ = ('entrypoint', 'main')
-
-
-def entrypoint():
-    import logging
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s [%(levelname)s] %(message)s',
-    )
-
-    return main()
+__all__ = ('main',)
