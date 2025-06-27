a4e56cbc3fd0f208819d6b124154da913c59e19d
Alastair Drong <adrong@gmail.com>
2025-06-27 12:49:23 -0700

    add some debugging output

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,6 +3,9 @@
 # The tests should live alongside this file.
 # Figure out where we are so we can let pytest know where to find the `wolnut` package.
 script_dir=$(cd `dirname $0` && pwd)
+cd "$script_dir"
+echo "Testing from directory: $script_dir"
+ls -lah
 
 # Run tests found in this dir and let pytest know that `wolnut` is at `../`
 pytest -o pythonpath="$script_dir/.." $@
