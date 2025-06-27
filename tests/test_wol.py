0dc5d9c8dac8105ca29a61cb372714d5255c849f
Alastair Drong <adrong@gmail.com>
2025-06-27 14:05:25 -0700

    reformatted using 'black'

--- tests/test_wol.py
+++ tests/test_wol.py
@@ -1,11 +1,11 @@
 def test_send_wol_packet(mocker):
-  # [ TODO - Issue #24 ] - Write tests that assert the appropriate exceptions were raised
+    # [ TODO - Issue #24 ] - Write tests that assert the appropriate exceptions were raised
 
-  # Normally, you'd patch an object. The funky order here is so that
-  # we can successfully patch a function. There's probably a better way to do this.
-  mock_send_magic_packet = mocker.patch("wakeonlan.send_magic_packet")
-  import wol
+    # Normally, you'd patch an object. The funky order here is so that
+    # we can successfully patch a function. There's probably a better way to do this.
+    mock_send_magic_packet = mocker.patch("wakeonlan.send_magic_packet")
+    import wol
 
-  # Intentionally using invalid values so an exception will be raised if the mock is wrong.
-  wol.send_wol_packet('testing', broadcast_ip='555.555')
-  mock_send_magic_packet.assert_called_once_with('testing', ip_address='555.555')
+    # Intentionally using invalid values so an exception will be raised if the mock is wrong.
+    wol.send_wol_packet("testing", broadcast_ip="555.555")
+    mock_send_magic_packet.assert_called_once_with("testing", ip_address="555.555")
